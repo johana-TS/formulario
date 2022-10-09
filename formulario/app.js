67f1f2c19d1f14da1ebe804d10fa156ab35c05eb
fabixx89 <115352471+fabixx89@users.noreply.github.com>
2022-10-09 17:01:50 -0300

    Formulario de contacto
    Diseñando el css para cel y javascrip.

---
+++ formulario/app.js
@@ -0,0 +1,39 @@
+const formulario = document.getElementById('form');
+const inputs = document.querySelectorAll("#form input");
+
+const expresiones = {
+    apyno: /^[a-zA-ZÁ-ÿ\s]{1,16}$/,
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+}
+
+const campos = {
+ apellido: false,
+ nombre: false,
+ correo: false,
+ comentario: false
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name){
+        case "apellido":
+            if (expresiones.apyno.test(e.target.value)){
+            console.log("caractervalido");
+            /*COLOCAR STYLE EXP. https://www.youtube.com/watch?v=s3pC93LgP18*/
+            }else{
+                console.log("carecter invalido");
+                document.getElementById("apellido");
+            /*COLOCAR STYLE*/
+            };
+        break;
+    };
+};
+inputs.forEach((input)=> {
+    input.addEventListener('keyup', validarFormulario);
+    /*input.addEventListener('blur', validarFormulario);*/
+});
+
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault()
+    alert("Formulario Enviado");
+});
